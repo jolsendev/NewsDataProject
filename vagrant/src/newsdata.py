1244a935b60cc7ac1ea49c7315444327ed3939eb
jolsendev <jolsendev@gmail.com>
2017-08-20 19:05:26 -0600

    removed notes

--- vagrant/src/newsdata.py
+++ vagrant/src/newsdata.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
 
-#
-# ef get_posts():
-#    """Return all posts from the 'database', most recent first."""
-#    conn = psycopg2.connect("dbname=forum")
-#   cur = conn.cursor()
-# cur.execute("SELECT content, time FROM posts order by time desc;")
-#    posts = cur.fetchall()
-#    conn.close()
-#    return posts
-
-
-# def add_post(content):
-#    """Add a post to the 'database' with the current timestamp."""
-#    bleached_content = bleach.clean(content)
-#    conn = psycopg2.connect("dbname=forum")
-#    cur = conn.cursor()
-#    cur.execute("INSERT INTO posts (content) VALUES (%s)", (bleached_content,))
-#    conn.commit()
-#    conn.close()
-
 import psycopg2
 
 COL_ONE = 0
